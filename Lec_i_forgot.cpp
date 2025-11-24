e473e8ecdb162b302d9322a54ac9f6af180a7f59
Jan <janek.czeladko@gmail.com>
2025-11-24 14:05:39 -0500

    new lecture type shi

---
+++ Lec_i_forgot.cpp
@@ -0,0 +1,16 @@
+/*
+Vectors
+
+Use the #include <vector> directive to include the vector library.
+
+List of useful functions:
+v[index] - access element at position index
+v.at(index) - access element at position index with error-bounds checking
+v.front() - access the first element
+v.back() - access the last element
+v.size() - returns the number of elements in the vector
+v.push_back(value) - adds value to the end of the vector
+v.pop_back() - removes the last element of the vector
+v.insert(position, value) - inserts value at the specified position
+
+*/
